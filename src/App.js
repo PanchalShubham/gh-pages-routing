ee3632e20f986155a5cb55f4e062181e0889c9ff
Shubham Panchal <shubhampanchal9773@gmail.com>
2021-02-08 12:53:32 +0530

    updated base-name

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-    <BrowserRouter>
+    <BrowserRouter basename="/gh-pages-routing">
       <header>
         <nav>
           <ul>
